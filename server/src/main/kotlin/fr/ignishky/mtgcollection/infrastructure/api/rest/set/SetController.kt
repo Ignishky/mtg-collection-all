bd05e132e3ec1c0f71a0c921cd633ebb020e0885
ignishky <thierry.ducloyer@gmail.com>
2023-09-06 23:10:28 +0200

    refactor(set): add log statement in controller

--- server/src/main/kotlin/fr/ignishky/mtgcollection/infrastructure/api/rest/set/SetController.kt
+++ server/src/main/kotlin/fr/ignishky/mtgcollection/infrastructure/api/rest/set/SetController.kt
@@ -6,6 +6,7 @@ import fr.ignishky.mtgcollection.domain.set.model.SetCode
 import fr.ignishky.mtgcollection.domain.set.port.SetApiPort
 import fr.ignishky.mtgcollection.infrastructure.api.rest.set.CardsResponse.CardResponse
 import fr.ignishky.mtgcollection.infrastructure.api.rest.set.SetsResponse.SetResponse
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,24 +15,32 @@ class SetController(
     private val cardApiPort: CardApiPort,
 ) : SetApi {
 
+    private val logger = KotlinLogging.logger {}
+
     override fun getAll(correlationId: CorrelationId): SetsResponse {
+        logger.info { "Requesting all sets ..." }
+
         val sets = setApiPort.getAll()
-        return SetsResponse(
-            sets.map { SetResponse(it.code.value, it.name.value, it.type.value, it.icon.value) }
-        )
+            .map { SetResponse(it.code.value, it.name.value, it.type.value, it.icon.value) }
+
+        logger.info { "Returning ${sets.size} sets." }
+        return SetsResponse(sets)
     }
 
     override fun getCards(correlationId: CorrelationId, setCode: String): CardsResponse {
+        logger.info { "Requesting all cards from '${setCode}'" }
+
         val cards = cardApiPort.getAll(SetCode(setCode))
-        return CardsResponse(
-            cards.map {
+            .map {
                 CardResponse(
                     it.name.value,
                     it.images.value[0].value,
                     CardsResponse.PricesResponse(it.prices.scryfall.eur, it.prices.scryfall.eurFoil)
                 )
             }
-        )
+
+        logger.info { "Returning ${cards.size} cards." }
+        return CardsResponse(cards)
     }
 
 }
