ccad4064cbbd2d744c673942b38577ab26415915
ignishky <thierry.ducloyer@gmail.com>
2023-09-10 14:18:17 +0200

    chore(deps): add actuator endpoints

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     runtimeOnly("org.flywaydb:flyway-core")
