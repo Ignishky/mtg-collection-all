4799a3bdecf8d77f423f0928af50dfe7b36f0a66
ignishky <thierry.ducloyer@gmail.com>
2023-09-17 11:32:05 +0200

    refactor(cqrs): remove unused dependencies

--- cqrs/build.gradle.kts
+++ cqrs/build.gradle.kts
@@ -3,9 +3,3 @@ plugins {
 }
 
 version = "1.0-SNAPSHOT"
-
-dependencies {
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-    implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    implementation("org.springframework:spring-jdbc:6.0.11")
-}
