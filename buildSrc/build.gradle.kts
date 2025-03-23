cf6024cb80c0e9c8a21e37bd0de2e6dd067e0682
ignishky <thierry.ducloyer@gmail.com>
2025-03-23 14:10:45 +0100

    chore(deps): bump org.jetbrains.kotlin:kotlin from 2.1.10 to 2.1.20

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.10")
-    implementation("org.jetbrains.kotlin:kotlin-allopen:2.1.10")
+    val kotlinVersion = "2.1.20"
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+    implementation("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:3.4.4")
 }
